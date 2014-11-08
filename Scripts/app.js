02264e76e6c32ffe6ac36ed254c2c049fbbabe26
Jesse <jesse.lloyd@students.fhu.edu>
2014-11-08 03:04:35 -0600

    Theme Redesign
    Overhaul of GoodStarter Theme to incorporate Bootstrap grid system and
    styling options.
    
    Changelog:
    - Site is now responsive. Tweaking will be required, but site should now
    look acceptable on all device sizes.
    - Added some JavaScript effects for navbar and cleaned navbar.
    - Began integration of Default.cshtml with the new _SiteLayout.
    - Placed OLD CSS file in "oldstyle.css". Going forward we will use
    "Site.css" - which contains all the current styling.
    
    Note: Pages will need to be refactored with new CSS, but this can be
    based on the old css in "oldstyle.css" which contains all our legacy css
    work for the project. This is to be completed by Wednesday.
    
    Added Signup static page (no current functionality). Basic - will need
    some work.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,21 +1,28 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    $(function() {  
-        var pull        = $('#pull');  
-            menu        = $('nav ul');  
-            menuHeight  = menu.height();  
-
-        $(pull).on('click', function(e) {  
-            e.preventDefault();  
-            menu.slideToggle();  
-        });  
-    }); 
-
-    $(window).resize(function(){  
-        var w = $(window).width();  
-        if(w > 320 && menu.is(':hidden')) {  
-            menu.removeAttr('style');  
-        }  
-    });
-    
+$(document).ready(function() {
+    $(window).scroll(function(){
+        console.log($(document).scrollTop());
+        
+        if ($(document).scrollTop() > 0)
+        {
+            $(".navbar-brand").css("font-size", "18px");
+            $(".navbar-brand").html("GoodStarter");
+            $(".navbar-default").css("opacity", "0.8");
+            $(".navbar-default").css("height", "50px");
+            $(".navbar-nav li a").css("display", "inline-block");
+            $(".navbar-brand").css("line-height", "20px");
+            $(".navbar-nav li a").css("line-height", "20px");
+            $(".navbar-form").css("line-height", "20px");
+        }
+        else
+        {
+            $(".navbar-brand").html("GoodStarter <i class=\"fa fa-leaf\"></i>");
+            $(".navbar-default").css("opacity", "1");
+            $(".navbar-brand").css("font-size", "36px");
+            $(".navbar-default").css("height", "80px");
+            $(".navbar-brand").css("line-height", "50px");
+            $(".navbar-nav li a").css("line-height", "50px");
+            $(".navbar-form").css("line-height", "60px");
+        }
+        
+        });
 });
